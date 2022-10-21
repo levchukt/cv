8da924c85f4713868b0b4667d53b8085f1dcf3af
levchukt <106012028+levchukt@users.noreply.github.com>
2022-10-21 23:49:10 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const tabNavItems = document.querySelectorAll('.tabs__button');
+
+const tabItems = document.querySelectorAll('.tabs__items');
+
+document.addEventListener('click', function (e) {
+  const targetElement = e.target;
+  let currentActiveIndex = null;
+  let newActiveIndex = null;
+  if (targetElement.closest('.tabs__button')) {
+    tabNavItems.forEach((tabNavItem, index) => {
+      if (tabNavItem.classList.contains('active')) {
+        currentActiveIndex = index;
+        tabNavItem.classList.remove('active');
+      }
+      if (tabNavItem === targetElement) {
+        newActiveIndex = index;
+      }
+    });
+    targetElement.classList.add('active');
+    tabItems[currentActiveIndex].classList.remove('active');
+    tabItems[newActiveIndex].classList.add('active');
+  }
+});
